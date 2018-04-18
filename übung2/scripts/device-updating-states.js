fad660adbc2673e9c6ef0e75983414381870bf15
Konrad Tim <e1267547@student.tuwien.ac.at>
2018-04-18 16:35:12 +0200

    intelligent conveyor & interim storage svg

--- übung2/scripts/device-updating-states.js
+++ übung2/scripts/device-updating-states.js
@@ -39,8 +39,8 @@ function updateItemGenerator(container, min, max, value) {
  */
 function updateMachine(container, min, max, value) {
     // TODO update svg: see assignment document
-    container.contents().find("#tspan3817").html(0);
-    container.contents().find("#tspan3817-6").html(100);
+    container.contents().find("#tspan3817").html(min);
+    container.contents().find("#tspan3817-6").html(max);
     if(min <= value && value < 34) {
         container.contents().find("#path3680").attr("style", "fill:#008800;fill-opacity:1");
         container.contents().find("#path3680").attr("d", "M 323.577 408.949 V 250.21899 H 254.64 L 253.93289 406.35969 C 229.95989 418.59369 214.201 444.10305 214.201 472.81205 214.201 513.56405 247.356 546.71805 288.109 546.71805 328.862 546.71805 362.017 513.56405 362.017 472.81205 362.017 444.10405 345.552 421.18405 321.577 408.94905 Z M 318.681 482.906 C 308.363 482.906 299.995 474.539 299.995 464.22 299.995 453.901 308.352 445.534 318.681 445.534 328.999 445.534 337.365 453.901 337.365 464.22 337.365 474.539 328.999 482.906 318.681 482.906 Z");
@@ -66,7 +66,7 @@ function updateMachine(container, min, max, value) {
 function updateConveyor(container, min, max, value) {
     // TODO update svg: see assignment document
 
-    if(value == true){
+    if(value){
       container.contents().find(".package").attr("opacity", "1");
     }else{
       container.contents().find(".package").attr("opacity", "0");
@@ -82,6 +82,13 @@ function updateConveyor(container, min, max, value) {
  */
 function updateIntelligentConveyor(container, min, max, value) {
     // TODO update svg: see assignment document
+    if(value){
+        container.contents().find(".packageLeft").attr("opacity", "1");
+        container.contents().find(".packageRight").attr("opacity", "1");
+    } else {
+        container.contents().find(".packageLeft").attr("opacity", "0");
+        container.contents().find(".packageRight").attr("opacity", "0");
+    }
 
 }
 
@@ -94,6 +101,15 @@ function updateIntelligentConveyor(container, min, max, value) {
  */
 function updateInterimStorage(container, min, max, value) {
     // TODO update svg: see assignment document
+    if(value < 5){
+        container.contents().find(".packageTop").attr("opacity", "0");
+        container.contents().find(".packageBottom").attr("opacity", "1");
+    } else {
+        container.contents().find(".packageTop").attr("opacity", "1");
+    }
+    if(value < 1){
+        container.contents().find(".packageBottom").attr("opacity", "0");
+    }
 
 }
 
