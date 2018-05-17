1770038461cf7a4080b4f0dde86c62f3013b82d1
Konrad Tim <e1267547@student.tuwien.ac.at>
2018-05-17 21:14:12 +0200

    anmeldebutton navigation, errot-text ausblenden

--- übung3/client/src/app/components/login/login.component.ts
+++ übung3/client/src/app/components/login/login.component.ts
@@ -1,8 +1,23 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+    display: boolean = false;
+
+    constructor(private router: Router) {
+    }
+
+    ngOnInit() {
+  }
+
+    onSubmit(form: NgForm) : void {
+        //this.display = true;
+        this.router.navigate(['/overview']);
+    }
 }
