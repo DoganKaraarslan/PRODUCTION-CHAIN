a0c6f982163b3e03d2371ad14aef73c8fa95f18a
Isikoglu Mustafa <musti.isi@hotmail.com>
2018-05-18 21:43:31 +0200

    client validation of options

--- übung3/client/src/app/components/options/options.component.ts
+++ übung3/client/src/app/components/options/options.component.ts
@@ -1,8 +1,18 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html'
 })
 export class OptionsComponent {
+
+  compare(form: NgForm): boolean {
+    return form.value["confirmPassword"] === form.value["password"];
+  }
+
+  onSubmit(form: NgForm) : void{
+    alert("Weiterschicken an Server zum Ändern -> ergebnis als Alert anzeigen.");
+  }
 }
