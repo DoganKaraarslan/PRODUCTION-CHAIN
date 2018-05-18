61bb7e3a15dbed89fdf9c19cc7013f9923052350
Konrad Tim <e1267547@student.tuwien.ac.at>
2018-05-18 17:50:28 +0200

    nav functions

--- übung3/client/src/app/components/nav/nav.component.ts
+++ übung3/client/src/app/components/nav/nav.component.ts
@@ -1,8 +1,32 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {OptionsComponent} from "../options/options.component";
+import {LoginComponent} from "../login/login.component";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html'
 })
 export class NavComponent {
+
+    constructor(private router: Router, private route: ActivatedRoute) {
+    };
+
+    isOptionsShown(): boolean {
+      return !this.isOptionsite() && !this.isLoginSite();
+    }
+
+    isLogoutShown(): boolean {
+      return !this.isLoginSite();
+    }
+
+
+    isOptionsite(): boolean {
+      return this.route.component === OptionsComponent;
+    }
+
+
+    isLoginSite(): boolean {
+      return this.route.component === LoginComponent;
+    }
 }
