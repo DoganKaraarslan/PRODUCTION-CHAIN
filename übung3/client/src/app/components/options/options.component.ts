88361ada58286dfd6c789e3d2002af520207841f
Karaarslan Dogan <e1529060@student.tuwien.ac.at>
2018-05-20 03:15:40 +0200

    options Daten werden gesendet

--- übung3/client/src/app/components/options/options.component.ts
+++ übung3/client/src/app/components/options/options.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import {PasswordChangeRequest} from '../../models/password.change.request';
 
 
 @Component({
@@ -8,11 +10,35 @@ import {NgForm} from '@angular/forms';
 })
 export class OptionsComponent {
 
-  compare(form: NgForm): boolean {
-    return form.value["confirmPassword"] === form.value["password"];
+  display: boolean = false;
+  passwords: PasswordChangeRequest = {oldPassword : "", newPassword : ""};
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
   }
 
   onSubmit(form: NgForm) : void{
     alert("Weiterschicken an Server zum Ändern -> ergebnis als Alert anzeigen.");
+
+
+    this.http.post('http://localhost:8081/changePassword', this.passwords, {responseType: 'text'}).subscribe(resp => {
+      if (eval(resp)){
+        this.display = false;
+      }else{
+        this.display = true;
+      }
+    });
   }
+
+
+    compare(form: NgForm): boolean {
+      return form.value["confirmPassword"] === form.value["password"];
+    }
+
+
+
+
 }
